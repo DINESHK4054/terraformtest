3f9a265379594db62aed1109ae17d8abe71e4f9d
DINESHK4054 <70650232+DINESHK4054@users.noreply.github.com>
2022-07-02 13:25:38 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,121 @@
-terraform {
-    required_providers {
-        aws={
-            source="hashicorp/aws"
-        }
-    }
-}
-provider "aws"{
-  profile = "default"
+# Configure the AWS Provider
+provider "aws" {
+  access_key = ""
+  secret_key = ""
   region  = "ap-southeast-1"
+}
+resource "aws_vpc" "vpc_devops" {
+  cidr_block       = "190.160.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc_devops"
+    Location ="Singapore"
+  }
+}
+ resource "aws_subnet" "sub_public_devops" {
+  vpc_id     = "${aws_vpc.vpc_devops.id}"
+  cidr_block = "190.160.1.0/24"
+
+ tags = {
+    Name = "sub_public_devops"
+    
+  }
+}
+resource "aws_security_group" "sg_devops" {
+  name        = "sg_devops"
+  description = "sg_devops_inbound traffic"
+  vpc_id      = "${aws_vpc.vpc_devops.id}"
+
+  ingress {
+    description = "sg_devops from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    description = "sg_devops from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-resource "aws_instance" "devbox" {
-    ami ="ami-02f26adf094f51167"
-    instance_type="t2.micro"
-  
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_devops"
+  }
+}
+
+resource "aws_instance" "kp_devops" {
+
+ami           = "ami-02b6d9703a69265e9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "kp_devops"
+  }
+}
+
+resource "aws_key_pair" "kp_devops" {
+  key_name   = "kp_devops-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
+resource "aws_launch_configuration" "kp_devops" {
+  name          = "kp_devops"
+  image_id      = "ami-02b6d9703a69265e9"
+  instance_type = "t2.micro"
+}
+resource "aws_elb" "bar" {
+  name               = "terraform-elb"
+  availability_zones = ["ap-southeast-1"]
+
+  access_logs {
+    bucket        = "foo"
+    bucket_prefix = "bar"
+    interval      = 60
+  }
+ listener {
+    instance_port     = 8080
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+listener {
+    instance_port      = 8080
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/terraform"
+}
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8080/"
+    interval            = 30
+  }
+ instances                   = [aws_instance.foo.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "terraform-elb"
+  }
 }
